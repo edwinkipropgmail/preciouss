373576bdce6e5c16cf03f7dcbaf59813dc1d5416
edwinkipropgmail <168726736+edwinkipropgmail@users.noreply.github.com>
2024-08-07 21:39:59 +0300

    Add files via upload

---
+++ precious.js
@@ -0,0 +1,13 @@
+'use strict'
+const options = document.querySelector('.bottom-bar-item1');
+const optionMenu = document.querySelector ('.options');
+const app = document.querySelector ('.app');
+const news = document.querySelector ('.news');
+const updates = document.querySelector ('.updates');
+const line = document.querySelector ('.line');
+const deposit = document.querySelector ('.dep');
+const send = document.querySelector ('.send');
+const body = document.querySelector ('body');
+
+
+options.addEventListener ('click', )
